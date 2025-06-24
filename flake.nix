ed94d4f21919b186b454b3d3c233aae5b32db395
Maximilian Homann <maximilian.homann@pionira-gmbh.de>
2025-06-24 16:58:26 +0200

    Applying formatter

--- flake.nix
+++ flake.nix
@@ -1,49 +1,51 @@
 {
-    description = "The Wolkenschloss flake";
+  description = "The Wolkenschloss flake";
 
-    inputs = {
-        # NixOS official packages
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
-        home-manager = {
-            url = "github:nix-community/home-manager/release-24.11";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
+  inputs = {
+    # NixOS official packages
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+  };
 
-    outputs = inputs@{
-        self,
-        nixpkgs,
-        nixos-wsl,
-        nixpkgs-unstable,
-        home-manager,
-        ...
-    }: {
-        formatter = {
-            x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nixos-wsl,
+      nixpkgs-unstable,
+      home-manager,
+      ...
+    }:
+    {
+      formatter = {
+        x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+      };
+      nixosConfigurations = {
+        nixos-testing-1 = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            nixos-wsl.nixosModules.default
+            {
+              system.stateVersion = "24.11";
+              wsl.enable = true;
+            }
+            ./machines/nixos-testing-1.nix
+          ];
         };
-        nixosConfigurations = {
-            nixos-testing-1 = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs; };
-                system = "x86_64-linux";
-                modules = [
-                    nixos-wsl.nixosModules.default
-                    {
-                        system.stateVersion = "24.11";
-                        wsl.enable = true;
-                    }
-                    ./machines/nixos-testing-1.nix
-                ];
-            };
 
-            backup-server = nixpkgs.lib.nixosSystem {
-                specialArgs = { inherit inputs; };
-                system = "x86_64-linux";
-                modules = [
-                    ./machines/backup-server.nix
-                ];
-            };
+        backup-server = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./machines/backup-server.nix
+          ];
         };
+      };
     };
 }
