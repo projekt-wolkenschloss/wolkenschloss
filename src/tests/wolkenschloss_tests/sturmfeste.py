79bd8817ecb31a288a35512762846855f894d0a0
Geothain <geothain@pm.me>
2026-07-08 23:06:22 +0200

    chore: adds test step todos

--- src/tests/wolkenschloss_tests/sturmfeste.py
+++ src/tests/wolkenschloss_tests/sturmfeste.py
@@ -1,9 +1,26 @@
 from test_driver.machine import QemuMachine
+from test_driver.driver import AssertionTester
 
-def start(vm_sturmfeste: QemuMachine, vm_other: QemuMachine) -> None:
+def start(tester: AssertionTester, vm_sturmfeste: QemuMachine, vm_other: QemuMachine) -> None:
     print("hey")
 
     vm_other.succeed("whoami")
     vm_sturmfeste.succeed("whoami")
 
     vm_other.succeed("stat /etc/dummy-data/precious-animals.txt")
+    
+    test_borg_backup(tester, vm_sturmfeste, vm_other)
+
+def test_borg_backup(tester: AssertionTester, vm_sturmfeste: QemuMachine, vm_other: QemuMachine) -> None:
+    # Copy or boot other VM with static test data
+    # Get checksums of test data
+    # Check that backup is loaded unit: timer, service
+    # Start backup
+    # Wait with timeout for unit to complete
+    # Check systemd status
+    # Parse borg repo contents for completeness
+    # Prepare restore folder
+    # Restore backup
+    # Checksum files and compare
+    _out = vm_other.succeed("sha256sum /etc/dummy-data/precious-animals.txt")
+    
